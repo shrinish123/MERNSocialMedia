b57db0876b4bbad69252d7010ce3886bf88cedd5
Shrinish Vhanbatte <shrinishiitkgp2001@gmail.com>
2022-02-14 00:16:38 +0530

    Changing baseURL

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://inder-social-media2.herokuapp.com";
+    : "https://newsocialmediashrinish.herokuapp.com";
 
 module.exports = baseUrl;
