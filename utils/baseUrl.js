a7904a9352239940712342b5d2dfb8946395a713
shrinish123 <shrinishiitkgp2001@gmail.com>
2023-12-15 19:35:43 +0530

    Deploying Ready

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://newsocialmediashrinish.herokuapp.com";
+    : "https://socialmediashrinish.netlify.app/";
+
 
 module.exports = baseUrl;
