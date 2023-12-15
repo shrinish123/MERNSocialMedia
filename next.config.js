ad726062f947ab0f5483c653b3ba132703949811
shrinish123 <shrinishiitkgp2001@gmail.com>
2023-12-15 21:54:06 +0530

    soved Cors issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports ={
     env:{
         CLOUDINARY_URL : "https://api.cloudinary.com/v1_1/dk96bclgg/image/upload/"
-    }
+    }, 
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://socialmediashrinish.netlify.app/:path*',
+          },
+        ]
+      },
 }
