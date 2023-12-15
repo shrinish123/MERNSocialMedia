bfe9be1d31acfe7eb9675edd0c4303a092e4e814
shrinish123 <shrinishiitkgp2001@gmail.com>
2023-12-15 22:36:38 +0530

    Solved issues

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports ={
     env:{
         CLOUDINARY_URL : "https://api.cloudinary.com/v1_1/dk96bclgg/image/upload/"
     }, 
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://socialmediashrinish.netlify.app/:path*',
+          },
+        ]
+      },
 }
