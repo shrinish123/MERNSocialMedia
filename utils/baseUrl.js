6bbc9d3203889076ff14ebf7ebc5cf84817c64e4
shrinish123 <shrinishiitkgp2001@gmail.com>
2023-12-20 02:40:34 +0530

    Fixed baseURL

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://socialmediashrinish.netlify.app";
+    : "https://socialconnectshrinish.vercel.app";
 
 
 module.exports = baseUrl;
